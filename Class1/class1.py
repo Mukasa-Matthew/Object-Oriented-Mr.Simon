de166a112853c2d955e8b702f5dc14b0106551b9
Mukasa Matthew <matthewmukasa0@gmail.com>
2025-09-12 11:45:47 +0300

    Add Profile class with OOP concepts - includes introduce(), show_stack(), and github_link() methods

--- Class1/class1.py
+++ Class1/class1.py
@@ -5,3 +5,8 @@ print("area of circle with radius", r1, "is", area_of_circle(r1))
 
 
 
+def area_of_rectangle(length, width):
+    return length * width
+l1 = 10
+w1 = 5
+print("area of rectangle with length", l1, "and width", w1, "is", area_of_rectangle(l1, w1))
